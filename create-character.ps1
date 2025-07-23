d864958e009025330b78dc71ecd9c1e38433d333
parking_turkeys <p-t@hackclub.app>
2025-07-23 16:13:33 -0400

    started work on the script [god i have to type these all up]

---
+++ create-character.ps1
@@ -0,0 +1,27 @@
+<#
+replacing lines:
+
+$text = "Lorem ipsum dolor sit amet"
+Write-Host -NoNewLine "$text`r"
+Start-Sleep -Seconds 2
+Write-Host -NoNewLine ("`b" * $text.Length)
+Write-Host "consectetur adipiscing elit"
+
+source: https://stackoverflow.com/questions/30240715/how-to-edit-the-text-that-was-already-written-to-console-with-powershell
+#>
+echo "Press Ctrl-C|Strg-C to stop at any point"
+$Species_Data = Get-Content "species-data.json"
+echo $Species_Data
+[int]$indexy = Read-Host "What species do you want your character to be? 
+0: Dwarf
+1: Elf
+2: Halfling
+3: Human
+4: Dragonborn
+5: Gnome
+6: Half-Elf
+7: Half-Orc
+8: Tiefling
+Answer"
+
+$Species_Data = $Species_Data[$indexy]
