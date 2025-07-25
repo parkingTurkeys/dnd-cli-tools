a02086fe78f67dbe12424fc53b7c7263a02b33eb
parking_turkeys <p-t@hackclub.app>
2025-07-25 16:30:22 -0400

    adding choosing subtypes

--- create-character.ps1
+++ create-character.ps1
@@ -11,7 +11,10 @@ source: https://stackoverflow.com/questions/30240715/how-to-edit-the-text-that-w
 #>
 echo "Press Ctrl-C|Strg-C to stop at any point"
 $Species_Data = Get-Content "species-data.json"
-echo $Species_Data
+#$Species_Data = ConvertTo-Json $Species_Data
+$Species_Data = $Species_Data | ConvertFrom-Json 
+
+# echo $Species_Data
 [int]$indexy = Read-Host "What species do you want your character to be? 
 0: Dwarf
 1: Elf
@@ -23,4 +26,16 @@ echo $Species_Data
 8: Tiefling
 Answer"
 
-$Species_Data = $Species_Data[$indexy]
+$Species_Data = $Species_Data.body[$indexy]
+
+if  ($Species_Data.Has_Subtype) {
+    for ($i = 0; $i -lt $Species_Data.Subtypes.length; $i++) {
+        $temp = $Species_Data.Subtypes[$i].Name
+        $formatted = "$formatted[$i]:$temp
+        "
+    }
+    [int]$indexy = Read-Host "What subtype do you want your character to be?
+    $formatted Answer" 
+    $Has_Subtype = true
+    $Subtype_Data =  $Species_Data.Subtypes[$indexy]
+}
