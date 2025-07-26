f0af38751928f5e5ebfb72547fb01af462c94714
parking_turkeys <p-t@hackclub.app>
2025-07-26 14:51:15 -0400

    commiting so it's clear what bug i was working on [i mixed up a , and a ; (T_T)]

--- create-character.ps1
+++ create-character.ps1
@@ -11,10 +11,18 @@ source: https://stackoverflow.com/questions/30240715/how-to-edit-the-text-that-w
 #>
 echo "Press Ctrl-C|Strg-C to stop at any point"
 $Species_Data = Get-Content "species-data.json"
-#$Species_Data = ConvertTo-Json $Species_Data
 $Species_Data = $Species_Data | ConvertFrom-Json 
+$number_words = @("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen")
+
+$Class_Data = Get-Content "class_data.json"
+$Class_Data = $Class_Data | ConvertFrom-Json 
+<#
+echo $Class_Data[0].Equipment[2].GetType()
+if ($Class_Data[0].Equipment[0].GetType().Name -eq "Object[]") {echo "hi"}
+#>
 
 # echo $Species_Data
+<#
 [int]$indexy = Read-Host "What species do you want your character to be? 
 0: Dwarf
 1: Elf
@@ -38,4 +46,34 @@ if  ($Species_Data.Has_Subtype) {
     $formatted Answer" 
     $Has_Subtype = true
     $Subtype_Data =  $Species_Data.Subtypes[$indexy]
+}
+#>
+
+[int]$indexy = Read-Host "What class do you want your character to be? 
+0: Barbarian
+1: Bard
+2: Cleric
+3: Druid
+4: Fighter
+5: Monk
+6: Paladin
+7: Ranger
+8: Rogue
+9: Sorcerer
+10: Warlock
+11: Wizard
+Answer"
+
+$Class_Data = $Class_Data[$indexy]
+$arraya = $Class_Data.Equipment.Length -split " "
+[int]$arraya = $arraya[0]
+for ($i = 0; $i -lt $arraya, $i++) {
+    if ($Class_Data.Equipment[$i].GetType().Name -eq "Object[]") {
+        # if it's an array
+        # turn both into strings
+        $first_choice = $number_words[$Class_Data.Equipment[$i][0].Amount] + " " + $Class_Data.Equipment[$i][0].Weapon
+        if ($Class_Data.Equipment[$i][0].Amount -gt 1) {$first_choice = $first_choice + "s"}
+        $second_choice = $number_words[$Class_Data.Equipment[$i][0].Amount] + " " + $Class_Data.Equipment[$i][0].Weapon
+        if ($Class_Data.Equipment[$i][0].Amount -gt 1) {$second_choice = $second_choice + "s"}
+    }
 }
